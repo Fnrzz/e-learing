a9c2284055e3ec86e7ea29347ba1305a5ab8880d
fnrzz <faridnurrganteng123@gmail.com>
2023-03-02 10:22:35 +0700

    [fix] migration and seeder course

--- database/migrations/2023_03_01_105305_create_courses_table.php
+++ database/migrations/2023_03_01_105305_create_courses_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('title');
+            $table->string('slug');
             $table->string('description');
             $table->string('link');
             $table->timestamps();
--- database/seeders/CourseSeeder.php
+++ database/seeders/CourseSeeder.php
@@ -14,25 +14,29 @@ class CourseSeeder extends Seeder
     public function run(): void
     {
         Course::create([
-            'name' => 'Perkenalan Laravel 8',
+            'title' => 'Perkenalan Laravel 8',
+            'slug' => 'perkenalan-laravel-8',
             'description' => 'Video perkenalan tentang laravel 8',
             'link' => 'https://www.youtube.com/embed/HqAMb6kqlLs'
         ]);
 
         Course::create([
-            'name' => 'Instalasi & Konfigurasi Laravel 8',
+            'title' => 'Instalasi & Konfigurasi Laravel 8',
+            'slug' => 'instalasi-&-konfigurasi-laravel-8',
             'description' => 'Video instalasi dan konfigurasi pada laravel 8',
             'link' => 'https://www.youtube.com/embed/pZqk57Xvujs'
         ]);
 
         Course::create([
-            'name' => 'Struktur Folder Pada Laravel 8',
+            'title' => 'Struktur Folder Pada Laravel 8',
+            'slug' => 'struktur-folder-pada-laravel-8',
             'description' => 'Video penjelasan struktur folder pada laravel 8',
             'link' => 'https://www.youtube.com/embed/u7zS2XpMpsc'
         ]);
 
         Course::create([
-            'name' => 'Templateing Engine Pada Laravel 8',
+            'title' => 'Templateing Engine Pada Laravel 8',
+            'slug' => 'templateing-engine-pada-laravel-8',
             'description' => 'Video penjelasan tentang templateing pada laravel 8',
             'link' => 'https://www.youtube.com/embed/9jrD0wcfq1g'
         ]);
